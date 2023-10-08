799fe1fdb791a08b19ca41b2f2941898e7262256
bidisha-15 <116992974+bidisha-15@users.noreply.github.com>
2023-10-09 01:22:32 +0530

    changes to cntributor section

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ setTimeout(() => {
     
    load.style.display="none";
 }, 3000);
+
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('contributors')
+const itemWidth = 150
+const padding = 10
+
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= (itemWidth + padding)
+})
+next.addEventListener('click',()=>{
+  list.scrollLeft += (itemWidth + padding)
+})
